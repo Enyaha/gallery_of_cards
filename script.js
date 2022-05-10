a7c35dcc330ef5fb33120d95cfb9511f50d4f06b
Evgenij <murzin90@yandex.ru>
2022-05-10 08:55:08 +0700

    start

---
+++ script.js
@@ -0,0 +1,21 @@
+function slidePlugin(slideActive = 0) {
+    const slides = document.querySelectorAll('.slide');
+    
+    slides[slideActive].classList.add('active');
+    
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+        clearActiveClasses();
+    
+            slide.classList.add('active');
+        })
+    }
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+    }   
+}
+
+slidePlugin(3)
